af828764954209a90f773a5522733b8f59fed7d6
RishitM <72683716+RishitM@users.noreply.github.com>
2021-04-15 19:24:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var noseX=0
 var noseY=0
 function preload(){
-mustache=loadImage("https://i.postimg.cc/3R4DFqrR/istockphoto-485318064-612x612-removebg-preview.png")
+mustache=loadImage("https://i.postimg.cc/TwPrTnYD/mask.png")
 }
 function setup(){
     canvas=createCanvas(300,300);
@@ -15,7 +15,7 @@ function setup(){
 }
 function draw(){
 image(video,0,0,300,300);
-image(mustache,noseX,noseY,80,80);
+image(mustache,noseX,noseY,250,300);
 }
 function takePic(){
 save("Now you are a clown");
@@ -25,10 +25,11 @@ function modelLoaded(){
 };
 function gotPoses(results){
     if (results.length>0){
-console.log(results);}
+console.log(results);
 noseX=results[0].pose.nose.x;
-noseX=noseX-35;
+noseX=noseX-125
 noseY=results[0].pose.nose.y;
-noseY=noseY-10;
+noseY=noseY-150
+    }
 console.log(noseX," is the x of the nose and, ",noseY,"is the y of the nose")
 }
